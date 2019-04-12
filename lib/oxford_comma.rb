1494bb00182607cdcbce2cea0e4b335102d51544
Tachi Shuttlesworth <tshuttlesworth@gmail.com>
2019-04-12 02:21:23 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
+  if array.length == 1
+    array.join
+  elsif array.length == 2
+    array.join(" and ")
+  else
+    array[0..-1].join(', ') + ", and " + array[-1]
+  end
+end 
 
-end
