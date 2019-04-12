a5bee69ef54e642491195a4f6e00c7e4662fed69
Tachi Shuttlesworth <tshuttlesworth@gmail.com>
2019-04-12 02:45:19 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
 def oxford_comma(array)
-
-end
+  if array.length == 1
+    array.join
+  elsif array.length == 2
+    array.join(" and ")
+  else
+    array[0..-2].join(', ') + ", and " + array[-1]
+  end
+end 
